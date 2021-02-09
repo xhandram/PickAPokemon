5b1745fe428bb49c24d9bf9c89fee01175f21d5a
Belladamix <belladamix@gmail.com>
2021-02-09 14:58:35 +0100

    manifest added and fetch listening

--- scripts.js
+++ scripts.js
@@ -8,12 +8,11 @@ window.addEventListener("load", (e) => {
 });
 
 pokemonlist.addEventListener("change", (e) => {
-  console.log(e.target.value);
-  getPokemon(getPokemon(e.target.value));
+  getPokemon(e.target.value);
 });
 
 async function getPokemonList() {
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=10";
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=30";
 
   const response = await fetch(url);
   const pokemons = await response.json();
@@ -24,10 +23,14 @@ async function getPokemonList() {
 }
 
 async function getPokemon(apiurl) {
-  const response = await fetch(apiurl);
-  const pokemon = await response.json();
-
-  pokemonCard.innerHTML = createPokemonCard(pokemon);
+  try {
+    const response = await fetch(apiurl);
+    const pokemon = await response.json();
+    pokemonCard.innerHTML = createPokemonCard(pokemon);
+  } catch (error) {
+    console.log("Error fetching data:", error);
+    pokemonCard.innerHTML = offlineCard();
+  }
 }
 
 function createPokemonCard(pokemon) {
@@ -39,7 +42,7 @@ function createPokemonCard(pokemon) {
   <div class="col-md-3 mx-auto m-3 ">
       <div class="card mx-auto" >
       <div class="card-header text-capitalize pt-1 pb-1 text-center"><h4>${pokemon.name}</h4></div>
-        <img src="${pokemon.sprites.other.dream_world.front_default}" class="text-center mx-auto d-block mt-3 mb-1" alt="${pokemon.name}" width="250px">
+        <img src="${pokemon.sprites.other.dream_world.front_default}" class="text-center mx-auto d-block mt-3 mb-1" alt="${pokemon.name}" width="250px" height="auto">
         <div class="card-body text-center text-capitalize p-4">
           <span class="badge bg-warning text-dark float-right">Height:${pokemon.height} </span>
           <span class="badge bg-info text-dark float-right">Weight: ${pokemon.weight}</span>
@@ -47,11 +50,31 @@ function createPokemonCard(pokemon) {
           <p class="card-text">
             <div class="descripcion ", sytle="font-size:16px;">
               <p><b>Pokemon Type</b></p>
-              <h4 class="text-danger">${pokTypes}</h4>
+              <h3 class="text-danger">${pokTypes}</h3>
 
             </div>
           </p>
           <a href="#" class="btn btn-primary">Get this Pokemon</a>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function offlineCard() {
+  return `
+  <div class="col-md-3 mx-auto m-3 ">
+      <div class="card mx-auto" >
+      <div class="card-header text-capitalize pt-1 pb-1 text-center"></div>
+       
+        <div class="card-body text-center text-capitalize p-4">
+
+          <p class="card-text">
+            <div class="descripcion ", sytle="font-size:16px;">
+              <p>Network Unavailable</p>
+
+            </div>
+          </p>         
         </div>
       </div>
     </div>
--- sw.js
+++ sw.js
@@ -4,10 +4,10 @@ const STATIC_RESOURCES = ["logo.png", "main.css", "scripts.js", "index.html"];
 //Install the service worker
 self.addEventListener("install", async (e) => {
   console.log("SW install");
+
   e.waitUntil(
     (async (e) => {
       const cache = await caches.open(CACHE_DATA);
-      console.log("In cache");
       return await cache.addAll(STATIC_RESOURCES);
     })()
   );
@@ -17,17 +17,14 @@ self.addEventListener("install", async (e) => {
 
 //Listen for fetching request
 self.addEventListener("fetch", async (e) => {
-  console.log(`SW fetch: ${e.request.url}`);
-
+  console.log("Fetching...");
   e.respondWith(
-    (async (e) => {
-      const cache = await open.caches(CACHE_DATA);
+    (async () => {
+      const cache = await caches.open(CACHE_DATA);
 
       try {
         const networkResponse = await fetch(e.request);
-        console.log(e.request);
         await cache.put(e.request, networkResponse.clone());
-        console.log("clone done");
         return networkResponse;
       } catch (error) {
         const cachedResponse = await cache.match(e.request);
